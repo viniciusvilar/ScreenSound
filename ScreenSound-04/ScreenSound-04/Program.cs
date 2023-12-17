b4c563b40000667f06e03c780761c97942416904
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2023-12-17 14:50:17 -0300

    Fazendo o consumo da API

---
+++ ScreenSound-04/ScreenSound-04/Program.cs
@@ -0,0 +1,26 @@
+﻿//using (HttpClient client = new HttpClient())
+//{
+//    try
+//    {
+//        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+//        Console.WriteLine(resposta);
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"Temos um problema: {ex.Message}");
+//    }
+    
+//}
+
+try
+{
+    for (int i = 3; i >= 0; i--)
+    {
+        Console.WriteLine($"Valor do I: {i}");
+        Console.WriteLine($"{10 / i}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Tivemos um problema: {ex.Message}");
+}
